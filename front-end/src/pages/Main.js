e36206036d2c6fa2e9b422f02f591be8f690a52c
roum02 <roum02@ewhain.net>
2021-10-30 03:13:43 +0900

    MainPage 추가

--- front-end/src/pages/Main.js
+++ front-end/src/pages/Main.js
@@ -1,8 +1,10 @@
 import React from "react";
+import { HashRouter as Router, Switch, Route, Link } from "react-router-dom";
+
 import styled from "styled-components";
 import Logo from "../assets/logo.png";
-import Text from "../components/Text";
 
+import Text from "../components/Text";
 import Button from "../components/Button";
 
 const Wrapper = styled.div`
