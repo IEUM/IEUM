9563ff32788379cea92f9fea8e6ad079f372e06b
roum02 <roum02@ewhain.net>
2021-11-30 15:12:52 +0900

    data 정제

--- back-end/ieum_data.sql
+++ back-end/ieum_data.sql
@@ -66,3 +66,49 @@ select city_name, gu_name, SUBSTRING_INDEX(gu_name, city_name, -1) from hospital
 
 select dong, temp_dong from hospital WHERE temp_gu = "강남구" 
 and temp_dong <> " " order by temp_dong;
+
+select * from hospital where city_code='' and temp_gu='' and temp_dong='';
+
+-- 카테고리
+alter table hospital add categories varchar(10);
+select hospital_name, categories from hospital limit 100;
+
+select hospital_name, categories from hospital 
+WHERE categories is null;
+
+UPDATE hospital SET categories = "내과"
+WHERE hospital_name like "%내과%";
+UPDATE hospital SET categories = "외과"
+WHERE hospital_name like "%외과%";
+UPDATE hospital SET categories = "이비인후과"
+WHERE hospital_name like "%이비인후과%";
+UPDATE hospital SET categories = "안과"
+WHERE hospital_name like "%안과%";
+UPDATE hospital SET categories = "신경외과"
+WHERE hospital_name like "%신경%";
+UPDATE hospital SET categories = "산부인과"
+WHERE hospital_name like "%산부인과%";
+UPDATE hospital SET categories = "성형외과"
+WHERE hospital_name like "%성형외과%";
+UPDATE hospital SET categories = "소아과"
+WHERE hospital_name like "%소아과%";
+UPDATE hospital SET categories = "소아과"
+WHERE hospital_name like "%소아%";
+UPDATE hospital SET categories = "정형외과"
+WHERE hospital_name like "%정형외과%";
+UPDATE hospital SET categories = "피부과"
+WHERE hospital_name like "%피부과%";
+UPDATE hospital SET categories = "비뇨기과"
+WHERE hospital_name like "%비뇨%";
+UPDATE hospital SET categories = "신경정신과"
+WHERE hospital_name like "%정신%";
+UPDATE hospital SET categories = "통증의학과"
+WHERE hospital_name like "%통증%";
+UPDATE hospital SET categories = "한의원"
+WHERE hospital_name like "%한의원%";
+UPDATE hospital SET categories = "한의원"
+WHERE hospital_name like "%한방%";
+UPDATE hospital SET categories = "치과"
+WHERE hospital_name like "%치과%";
+
+
--- front-end/src/pages/data/categories.js
+++ front-end/src/pages/data/categories.js
@@ -1,15 +1,15 @@
 const categories = [
   { id: 0, text: "전체" },
   { id: 1, text: "내과" },
-  { id: 2, text: "의과" },
+  { id: 2, text: "외과" },
   { id: 3, text: "이비인후과" },
-  { id: 4, text: "신경외과" },
-  { id: 5, text: "정형외과" },
+  { id: 4, text: "안과" },
+  { id: 5, text: "신경외과" },
   { id: 6, text: "산부인과" },
   { id: 7, text: "성형외과" },
-  { id: 8, text: "피부과" },
-  { id: 9, text: "안과" },
-  { id: 10, text: "소아과" },
+  { id: 8, text: "소아과" },
+  { id: 9, text: "정형외과" },
+  { id: 10, text: "피부과" },
 ];
 
 export default categories;
