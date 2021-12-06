70c606a2b93b652b6b3d36e3568f802e53a9f44a
roum02 <roum02@ewhain.net>
2021-12-07 03:21:59 +0900

    map marker가 돌아는 감;

--- front-end/src/pages/Map.js
+++ front-end/src/pages/Map.js
@@ -21,29 +21,75 @@ const MapBox = styled.div`
   height: 90vh;
 `;
 
-const { kakao } = window;
+//const { kakao } = window;
 
 const Map = ({ location }) => {
   const where = location.state.where;
   const result = location.state.result;
 
+  console.log(result);
+
   useEffect(() => {
+    mapScript();
+  }, []);
+
+  const mapScript = () => {
     let container = document.getElementById("map");
     let options = {
-      center: new kakao.maps.LatLng(37.365264512305174, 127.10676860117488),
+      center: new kakao.maps.LatLng(result[0].y, result[0].x),
       level: 3,
     };
-    let map = new kakao.maps.Map(container, options);
 
-    let markerPosition = new kakao.maps.LatLng(
-      37.365264512305174,
-      127.10676860117488
-    );
-    let marker = new kakao.maps.Marker({
-      position: markerPosition,
+    //map
+    const map = new kakao.maps.Map(container, options);
+    result.forEach((el) => {
+      // 마커를 생성합니다
+      const marker = new kakao.maps.Marker({
+        //마커가 표시 될 지도
+        map: map,
+        //마커가 표시 될 위치
+        position: new kakao.maps.LatLng(el.y, el.x),
+        title: el.hospital_name,
+        clickable: true,
+      });
+
+      // 마커에 표시할 인포윈도우를 생성합니다
+      let iwContent = "<div>{el.hospital_name}안녕</div>",
+        iwRemoveable = true;
+
+      const infowindow = new kakao.maps.InfoWindow({
+        content: iwContent, // 인포윈도우에 표시할 내용
+        removable: iwRemoveable,
+      });
+
+      // 마커에 mouseover 이벤트와 mouseout 이벤트를 등록합니다
+      // 이벤트 리스너로는 클로저를 만들어 등록합니다
+      // 클로저를 만들어 주지 않으면 마지막 마커에만 이벤트가 등록됩니다
+      kakao.maps.event.addListener(
+        marker,
+        "mouseover",
+        makeOverListener(map, marker, infowindow)
+      );
+      kakao.maps.event.addListener(
+        marker,
+        "mouseout",
+        makeOutListener(infowindow)
+      );
     });
-    marker.setMap(map);
-  }, []);
+    // 인포윈도우를 표시하는 클로저를 만드는 함수입니다
+    function makeOverListener(map, marker, infowindow) {
+      return function () {
+        infowindow.open(map, marker);
+      };
+    }
+
+    // 인포윈도우를 닫는 클로저를 만드는 함수입니다
+    function makeOutListener(infowindow) {
+      return function () {
+        infowindow.close();
+      };
+    }
+  };
 
   return (
     <Wrapper alignItems="none">
